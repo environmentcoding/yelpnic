5e04fa6e5019e5ea7aade19043908b8f820ad699
Niccolo Paparella <environmentcoding@gmail.com>
2019-09-13 17:41:49 +0000

    caption error still working

--- app/models/place.rb
+++ app/models/place.rb
@@ -14,10 +14,12 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
-  has_many :caption
+  has_many :photos
 
   geocoded_by :address
   after_validation :geocode
 
   validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
